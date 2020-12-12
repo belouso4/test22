fcf98de34a46706f836f909bf48d49b39348f592
kirill <kirill.bielousov15151515@gmail.com>
2020-12-12 18:17:07 +0300

    commit

--- dist/assets/api/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$link = mysqli_connect("localhost", "root", "", "shop") or die("Sorry, can't connect to the mysql.");
--- src/assets/api/email.php
+++ src/assets/api/email.php
@@ -9,8 +9,8 @@ $message = ' <p>Текст письма</p> </br> <b>1-ая строчка </b> 
 $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
 $headers .= "From: От кого письмо <from@example.com>\r\n";
 $headers .= "Reply-To: reply-to@example.com\r\n";
-
+echo $_POST['name'];
+echo $_POST['phone'];
 mail($to, $subject, $message, $headers);
 header('Location: http://belouso4.beget.tech/');
 exit;
-?>
